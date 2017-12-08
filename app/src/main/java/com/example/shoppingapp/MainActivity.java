e39027d0107bd8440640de1ac8886a4218dc1ba0
Nathan <rgraves.math@gmail.com>
2017-12-08 12:26:16 -0600

    Basic app

--- app/src/main/java/com/example/shoppingapp/MainActivity.java
+++ app/src/main/java/com/example/shoppingapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.shoppingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+        //The button name does not have to be the same as the button being reference but it helps to be uniform.
+        //in the findViewById R stands for resources.  The code lets us search our resources for an element by it's ID.
+        //We use the (Button) to cast the result if the findViewById as a button.
+
+        //Now we'll create an onClick listener.  This will listen for any time the button specified is clicked to perform the action we request.
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {//This is the onClick Event.  All of our code has to go in here for the click.
+                //Here we create java objects for both the EditText fields using the EditText object type.  As before these java objects do not need to
+                //be of the same name as the android ID, but it helps to be consistent.
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+                //As the above are actually text strings we now need to parse it to get the integer for calculations.
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result = num1 + num2;
+                //easiest way to change result to a text is to concat it with an empty string.  We do so when setting the resultTextView below
+                resultTextView.setText(result + "");
+            }
+        });
     }
 }
